bb600a338f309b12b4041ca575b27f4283d67ac7
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-03-28 13:06:29 +0700

    example

---
+++ copy-file.js
@@ -0,0 +1,18 @@
+﻿// @ts-check
+const ncp = require('ncp').ncp;
+const path = require('path');
+
+const source_dir = path.join(__dirname, 'path', 'to', 'source');
+
+const dest_dir = path.join(process.cwd(), 'path', 'to', 'destination');
+
+const options = {
+	clobber: true,
+};
+
+ncp(source_dir, dest_dir, options, function (err) {
+	if (err) {
+		return console.error(err);
+	}
+	console.log('example files copied successfully!');
+});
