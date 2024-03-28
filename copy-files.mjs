6ccca200559a91285ba7e98b98ea18f814fdb4d3
hakim_jazuli <muhammadhakimjazuli@gmail.com>
2024-03-28 13:38:40 +0700

    test

--- copy-files.mjs
+++ copy-files.mjs
@@ -1,6 +1,8 @@
 ﻿// @ts-check
-const ncp = require('ncp').ncp;
-const path = require('path');
+import ncp from 'ncp';
+import path from 'path';
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const folder_ = 'h_modules_example';
 const source_dir = path.join(__dirname, folder_);
 const dest_dir = path.join(process.cwd(), folder_);
@@ -9,7 +11,7 @@ const options = {
 	clobber: true,
 };
 
-ncp(source_dir, dest_dir, options, function (err) {
+ncp.ncp(source_dir, dest_dir, options, function (err) {
 	if (err) {
 		return console.error(err);
 	}
